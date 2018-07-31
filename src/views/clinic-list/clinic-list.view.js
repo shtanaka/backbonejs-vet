bcb4832c22ce9916de3c30d9dbce772c761a4417
Gustavo Santiago <egustavo4@gmail.com>
2018-07-31 04:10:40 -0300

    Add bower.js; separate project in files

--- src/views/clinic-list/clinic-list.view.js
+++ src/views/clinic-list/clinic-list.view.js
@@ -1,11 +1,8 @@
-// ClinicsApp.scope.clinics = new ClinicsApp.collections.Clinics([]);
-// ClinicsApp.scope.clinics.fetch();
-
 ClinicsApp.views.ClinicListItemView = Backbone.View.extend({
   model: new ClinicsApp.models.Clinic(),
   tagName: 'tr',
   initialize: function () {
-    this.template = _.template($('#clinicListTemplate').html());
+    this.template = _.template($('.clinics-list-item-template').html());
   },
   render: function () {
     var html = this.template(this.model.toJSON());
@@ -14,18 +11,3 @@ ClinicsApp.views.ClinicListItemView = Backbone.View.extend({
   }
 });
 
-ClinicsApp.views.ClinicListView = Backbone.View.extend({
-  model: ClinicsApp.scope.clinics,
-  el: $('.clinics-list'),
-  initialize: function () {
-    this.model.on('add', this.render, this);
-  },
-  render: function () {
-    var self = this;
-    this.$el.html('');
-    _.each(this.model.toArray(), function (data) {
-      self.$el.append((new ClinicListItemView({ model: data })).render().$el);
-    });
-    return this;
-  }
-});
--- src/views/home/home.view.js
+++ src/views/home/home.view.js
@@ -1,7 +1,20 @@
+ClinicsApp.scope.clinics = new ClinicsApp.collections.Clinics([]);
+
 ClinicsApp.views.Home = Backbone.View.extend({
-  initialize: function (options) { },
+  model: ClinicsApp.scope.clinics,
+  initialize: function (options) {
+    this.model.on('add', this.render, this);
+    this.model.fetch();
+  },
+  template: _.template($('.home-template').html()),
+  target: $('.clinics-list'),
   render: function () {
-    this.$el.html("This is my home page!");
+    this.$el.html(this.template());
+    var clinicsList = this.$el.find('.clinics-list');
+    clinicsList.html('');
+    _.each(this.model.toArray(), function (data) {
+      clinicsList.append((new ClinicsApp.views.ClinicListItemView({ model: data })).render().$el);
+    });
     return this;
   }
 });
